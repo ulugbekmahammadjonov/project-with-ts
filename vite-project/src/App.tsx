c3dac4c27ba552c4f9fe0faae0fa41399899a3fd
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-10-31 21:05:33 +0500

    lesson52

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,24 @@
+import {Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Store from "./pages/Store"
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/store" element={<Store/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
